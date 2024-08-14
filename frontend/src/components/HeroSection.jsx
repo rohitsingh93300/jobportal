aa5453594c7c31796d384fc8d0cf1671ac5577d9
rohitsingh93300 <rohitsingh93300@gmail.com>
2024-08-14 21:42:55 +0530

    input colour updated

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
                 <span className='mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
                 <h1 className='text-4xl md:text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel provident sapiente libero nostrum praesentium?</p>
-                <div className='flex w-[70%] md:w-[40%] shadow-lg border border-gray-20 pl-3 rounded-full items-center gap-4 mx-auto'>
+                <div className='flex w-[70%] md:w-[40%] shadow-lg bg-white border border-gray-20 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input
                         type="text"
                         placeholder='Find your dream jobs'
